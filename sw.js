b86ad3a89c0e97e8c475615f2dcecd7d8cfe5c1c
tnpscachievers007-wq <tnpscachievers007@gmail.com>
2026-09-24 19:38:04 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "steno-drill-v1";
+const CACHE = "steno-drill-v2";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", function (e) {
